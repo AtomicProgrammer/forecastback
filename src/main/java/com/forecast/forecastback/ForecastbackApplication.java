787163d0c2fb0b8f239662a564b38ab95f5dc3c1
shaotianqiang <732010807@qq.com>
2018-11-13 18:36:42 +0800

    Initial commit

---
+++ src/main/java/com/forecast/forecastback/ForecastbackApplication.java
@@ -0,0 +1,12 @@
+package com.forecast.forecastback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForecastbackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ForecastbackApplication.class, args);
+  }
+}
---
+++ src/test/java/com/forecast/forecastback/ForecastbackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.forecast.forecastback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ForecastbackApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
